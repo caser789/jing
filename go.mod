5e61575b4d38892dad253052dfbb7878de6c7e05
Xue Jiao <jiao.xue@seamoney.com>
2023-02-06 20:38:19 +0800

    send/receive TCP packet

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module jing
 
 go 1.17
+
+require (
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"math/rand"
 	"net"
 )
 
 type Pinger struct {
-	addr   string
-	ipaddr *net.IPAddr
+	sourceAddr string
+	addr       string
+	ipaddr     *net.IPAddr
 }
 
 func (p *Pinger) SetAddr(addr string) error {
@@ -27,6 +31,46 @@ func (p *Pinger) SetIPAddr(ipaddr *net.IPAddr) {
 }
 func (p *Pinger) IPAddr() *net.IPAddr { return p.ipaddr }
 
+func (p *Pinger) Run() error {
+	conn, err := icmp.ListenPacket("ip4:icmp", p.sourceAddr)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := (&icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID:   rand.Intn(65535),
+			Seq:  1,
+			Data: []byte{11, 22},
+		},
+	}).Marshal(nil)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("bytes sent %s\n", bytes)
+	length, err := conn.WriteTo(bytes, p.ipaddr)
+	// length, err := conn.WriteTo(bytes, &net.IPAddr{IP: net.IPv4(192, 168, 1, 93)})
+	if err != nil {
+		return err
+	}
+
+	bytesGot := make([]byte, 512)
+	n, _, err := conn.ReadFrom(bytesGot)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("bytes received %s, %d\n", bytesGot, n)
+	rm, err := icmp.ParseMessage(1, bytesGot[:n])
+	if err != nil {
+		return err
+	}
+	fmt.Printf("bytes received %v, %d\n", rm, n)
+
+	fmt.Printf("conn write to length %d\n", length)
+	return nil
+}
+
 func main() {
 	pinger := &Pinger{}
 	err := pinger.SetAddr("www.google.com")
@@ -35,4 +79,7 @@ func main() {
 		return
 	}
 	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+
+	err = pinger.Run()
+	fmt.Printf("run error %s", err)
 }
