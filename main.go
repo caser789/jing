c952c3831ab55915c960d05a30312992b66ce3e8
Xue Jiao <jiao.xue@seamoney.com>
2023-02-06 22:21:52 +0800

    init command line tool

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 	"math/rand"
 	"net"
+	"time"
 )
+
+func NewPinger(host string) (*Pinger, error) {
+	p := &Pinger{}
+	err := p.SetAddr(host)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
 
 type Pinger struct {
 	sourceAddr string
@@ -31,55 +42,102 @@ func (p *Pinger) SetIPAddr(ipaddr *net.IPAddr) {
 }
 func (p *Pinger) IPAddr() *net.IPAddr { return p.ipaddr }
 
-func (p *Pinger) Run() error {
+func (p *Pinger) Run() {
 	conn, err := icmp.ListenPacket("ip4:icmp", p.sourceAddr)
 	if err != nil {
-		return err
-	}
-
-	bytes, err := (&icmp.Message{
-		Type: ipv4.ICMPTypeEcho, Code: 0,
-		Body: &icmp.Echo{
-			ID:   rand.Intn(65535),
-			Seq:  1,
-			Data: []byte{11, 22},
-		},
-	}).Marshal(nil)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("bytes sent %s\n", bytes)
-	length, err := conn.WriteTo(bytes, p.ipaddr)
-	// length, err := conn.WriteTo(bytes, &net.IPAddr{IP: net.IPv4(192, 168, 1, 93)})
-	if err != nil {
-		return err
-	}
-
-	bytesGot := make([]byte, 512)
-	n, _, err := conn.ReadFrom(bytesGot)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("bytes received %s, %d\n", bytesGot, n)
-	rm, err := icmp.ParseMessage(1, bytesGot[:n])
-	if err != nil {
-		return err
-	}
-	fmt.Printf("bytes received %v, %d\n", rm, n)
-
-	fmt.Printf("conn write to length %d\n", length)
-	return nil
-}
-
-func main() {
-	pinger := &Pinger{}
-	err := pinger.SetAddr("www.google.com")
-	if err != nil {
-		fmt.Printf("set addr err: %s\n", err)
+		fmt.Printf("ListenPacket error %s\n", err)
 		return
 	}
-	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
 
-	err = pinger.Run()
-	fmt.Printf("run error %s", err)
+	// 不断 发送和接收
+	// 接收
+	go func() {
+		for {
+			bytesGot := make([]byte, 512)
+			n, _, err := conn.ReadFrom(bytesGot)
+			if err != nil {
+				return
+			}
+
+			rm, err := icmp.ParseMessage(1, bytesGot[:n])
+			if err != nil {
+				return
+			}
+
+			pkt := rm.Body.(*icmp.Echo)
+			Rtt := time.Since(bytesToTime(pkt.Data[:8]))
+			fmt.Printf("RTT is %s\n", Rtt)
+		}
+	}()
+
+	// 发送
+	for {
+		bytes, err := (&icmp.Message{
+			Type: ipv4.ICMPTypeEcho, Code: 0,
+			Body: &icmp.Echo{
+				ID:   rand.Intn(65535),
+				Seq:  1,
+				Data: timeToBytes(time.Now()),
+			},
+		}).Marshal(nil)
+		if err != nil {
+			time.Sleep(time.Second * 10)
+			continue
+		}
+
+		_, err = conn.WriteTo(bytes, p.ipaddr)
+		// length, err := conn.WriteTo(bytes, &net.IPAddr{IP: net.IPv4(192, 168, 1, 93)})
+		if err != nil {
+			time.Sleep(time.Second * 10)
+			continue
+		}
+
+		time.Sleep(time.Second * 10)
+	}
+}
+
+var usage = `
+Usage:
+    ping host
+Examples:
+    # ping google continuously
+    ping www.google.com
+`
+
+func main() {
+	flag.Usage = func() {
+		fmt.Printf(usage)
+	}
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		return
+	}
+	host := flag.Arg(0)
+	pinger, err := NewPinger(host)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err.Error())
+		return
+	}
+
+	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+	pinger.Run()
+}
+
+func timeToBytes(t time.Time) []byte {
+	nsec := t.UnixNano()
+	b := make([]byte, 8)
+	for i := uint8(0); i < 8; i++ {
+		b[i] = byte((nsec >> ((7 - i) * 8)) & 0xff)
+	}
+	return b
+}
+
+func bytesToTime(b []byte) time.Time {
+	var nsec int64
+	for i := uint8(0); i < 8; i++ {
+		nsec += int64(b[i]) << ((7 - i) * 8)
+	}
+	return time.Unix(nsec/1000000000, nsec%1000000000)
 }
