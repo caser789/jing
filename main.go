b0b3978dd54ca5af3a4485e1da4ae31127f3b973
Xue Jiao <jiao.xue@seamoney.com>
2023-02-09 21:27:08 +0800

    print stat on finish

--- main.go
+++ main.go
@@ -3,17 +3,22 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
+	"math"
 	"math/rand"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func NewPinger(host string) (*Pinger, error) {
-	p := &Pinger{}
+	p := &Pinger{stat: &Stat{}}
 	err := p.SetAddr(host)
 	if err != nil {
 		return nil, err
@@ -25,6 +30,9 @@ type Pinger struct {
 	sourceAddr string
 	addr       string
 	ipaddr     *net.IPAddr
+
+	seq  int
+	stat *Stat
 
 	packetRecv int
 
@@ -50,7 +58,61 @@ func (p *Pinger) SetIPAddr(ipaddr *net.IPAddr) {
 }
 func (p *Pinger) IPAddr() *net.IPAddr { return p.ipaddr }
 
+func (p *Pinger) finish() {
+	stat := p.Stat()
+	fmt.Printf("\n--- %s ping statistics ---\n", p.Addr())
+	fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n", stat.PacketsSent, stat.PacketsRecv, stat.PacketLoss)
+	fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n", stat.MinRtt, stat.AvgRtt, stat.MaxRtt, stat.StdDevRtt)
+}
+
+// Stat represent the stats of a currently running or finished
+// pinger operation.
+type Stat struct {
+	// PacketsRecv is the number of packets received.
+	PacketsRecv int
+
+	// PacketsSent is the number of packets sent.
+	PacketsSent int
+
+	// PacketLoss is the percentage of packets lost.
+	PacketLoss float64
+
+	// Rtts is all of the round-trip times sent via this pinger.
+	Rtts []time.Duration
+
+	// MinRtt is the minimum round-trip time sent via this pinger.
+	MinRtt time.Duration
+
+	// MaxRtt is the maximum round-trip time sent via this pinger.
+	MaxRtt time.Duration
+
+	// AvgRtt is the average round-trip time sent via this pinger.
+	AvgRtt time.Duration
+
+	// SumRtt is the average round-trip time sent via this pinger.
+	SumRtt time.Duration
+
+	// SumSqrtRtt is the average round-trip time sent via this pinger.
+	SumSqrtRtt time.Duration
+
+	// StdDevRtt is the standard deviation of the round-trip times sent via
+	// this pinger.
+	StdDevRtt time.Duration
+}
+
+func (p *Pinger) Stat() *Stat {
+	p.stat.PacketLoss = float64(p.stat.PacketsSent-p.stat.PacketsRecv) / float64(p.stat.PacketsSent) * 100
+	if len(p.stat.Rtts) > 0 {
+		p.stat.StdDevRtt = time.Duration(math.Sqrt(float64(p.stat.SumSqrtRtt / time.Duration(len(p.stat.Rtts)))))
+	}
+	return p.stat
+}
+
 func (p *Pinger) Run() {
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), p.Timeout)
+	defer cancel()
+	defer p.finish()
+
 	conn, err := icmp.ListenPacket("ip4:icmp", p.sourceAddr)
 	if err != nil {
 		fmt.Printf("ListenPacket error %s\n", err)
@@ -75,7 +137,18 @@ func (p *Pinger) Run() {
 			pkt := rm.Body.(*icmp.Echo)
 			Rtt := time.Since(bytesToTime(pkt.Data[:8]))
 			p.packetRecv++
-			fmt.Printf("RTT is %s\n", Rtt)
+			p.stat.PacketsRecv++
+			p.stat.Rtts = append(p.stat.Rtts, Rtt)
+			if Rtt > p.stat.MaxRtt {
+				p.stat.MaxRtt = Rtt
+			}
+			if p.stat.MinRtt == 0 || Rtt < p.stat.MinRtt {
+				p.stat.MinRtt = Rtt
+			}
+			p.stat.SumRtt += Rtt
+			p.stat.AvgRtt = p.stat.SumRtt / time.Duration(len(p.stat.Rtts))
+			p.stat.SumSqrtRtt += (Rtt - p.stat.AvgRtt) * (Rtt - p.stat.AvgRtt)
+			fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n", n, p.ipaddr, pkt.Seq, Rtt)
 			if p.packetRecv == p.Count {
 				close(closed)
 				return
@@ -85,10 +158,14 @@ func (p *Pinger) Run() {
 
 	_ = p.sendICMP(conn)
 	interval := time.NewTicker(p.Interval)
-	timeout := time.NewTicker(p.Timeout)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
 	for {
 		select {
-		case <-timeout.C:
+		case <-c:
+			return
+		case <-ctxTimeout.Done():
 			return
 		case <-interval.C:
 			_ = p.sendICMP(conn)
@@ -103,13 +180,15 @@ func (p *Pinger) sendICMP(conn *icmp.PacketConn) error {
 		Type: ipv4.ICMPTypeEcho, Code: 0,
 		Body: &icmp.Echo{
 			ID:   rand.Intn(65535),
-			Seq:  1,
+			Seq:  p.seq,
 			Data: timeToBytes(time.Now()),
 		},
 	}).Marshal(nil)
 	if err != nil {
 		return err
 	}
+	p.seq++
+	p.stat.PacketsSent++
 
 	_, err = conn.WriteTo(bytes, p.ipaddr)
 	return err
@@ -158,7 +237,7 @@ func main() {
 	pinger.Count = *count
 	pinger.Interval = *interval
 	pinger.Timeout = *timeout
-	fmt.Printf("PING %s (%s) count=%d:\n", pinger.Addr(), pinger.IPAddr(), *count)
+	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
 	pinger.Run()
 }
 
